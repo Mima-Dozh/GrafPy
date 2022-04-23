d30ef177d2c5e059ef70587347150733770bd3bf
Mima-Dozh <d.k.mozhaev12345@gmail.com>
2022-04-23 10:12:32 +0500

    Add top sort

--- dfs.py
+++ dfs.py
@@ -1,6 +1,16 @@
 used = {} 
+ans = []
 def DFS(a, start):
+    print(used)
     used[start] = True
-    for i in range(a[start]):
-        if not used.ContainsKey(i):
+    for i in a[start]:
+        if not used.get(i):
             DFS(a, i)
+    ans.append(start)
+
+def topological_sort(a):
+    used.clear()
+    for i in range(len(a)):
+        if not used.get(i):
+            DFS(a, i)
+    return ans[::-1]
